3e3ab0c953c7e1713d565f67bc7f236d72a78a06
Iván López <lopez.ivan@gmail.com>
2019-03-26 10:53:39 +0100

    Add views and Thymeleaf support

---
+++ src/main/java/example/micronaut/User.java
@@ -0,0 +1,31 @@
+package example.micronaut;
+
+public class User {
+
+    private String username;
+    private Boolean loggedIn;
+
+    public User() {
+    }
+
+    public User(String username, boolean loggedIn) {
+        this.username = username;
+        this.loggedIn = loggedIn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Boolean getLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+}
---
+++ src/main/java/example/micronaut/ViewsController.java
@@ -0,0 +1,14 @@
+package example.micronaut;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.views.ModelAndView;
+
+@Controller("/views")
+public class ViewsController {
+
+    @Get("/")
+    public ModelAndView<User> modelAndView() {
+        return new ModelAndView<>("home", new User("Iván", true));
+    }
+}
