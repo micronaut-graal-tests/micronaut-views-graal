9a711661f74f05278f678a6b2a549fa6c9bdde36
Iván López <lopez.ivan@gmail.com>
2019-05-02 16:41:42 +0200

    Use Micronaut 1.1.1-graal.BUILD-SNAPSHOT that removes the deprecated flags

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/views-graal-*.jar --initialize-at-build-time=ch.qos.logback,com.fasterxml.jackson,fresh.graal,io.micronaut,io.netty,io.reactivex,org.reactivestreams,org.slf4j,org.yaml.snakeyaml,javax
+native-image --no-server --no-fallback --class-path build/libs/views-graal-*.jar
