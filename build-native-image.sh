1625efd5d2617c254d62a3d2cb922b79a84a7157
Iván López <lopez.ivan@gmail.com>
2019-05-21 14:55:48 +0200

    Add Handlebars

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/views-graal-*.jar --initialize-at-build-time=javassist.ClassPool
+native-image --no-server --no-fallback --class-path build/libs/views-graal-*.jar --initialize-at-build-time=com.github.jknack.handlebars
