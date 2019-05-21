e0583cd142e2d23c1dd337e7d93341c6c0e4d8f3
Iván López <lopez.ivan@gmail.com>
2019-05-21 17:23:12 +0200

    Add Velocity

--- src/main/java/example/micronaut/ViewsController.java
+++ src/main/java/example/micronaut/ViewsController.java
@@ -1,14 +1,22 @@
 package example.micronaut;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.ModelAndView;
+import io.micronaut.views.View;
 
 @Controller("/views")
 public class ViewsController {
 
-    @Get("/")
-    public ModelAndView<User> modelAndView() {
-        return new ModelAndView<>("dir/home", new User("Iván", true));
+    @Get("/velocity")
+    public ModelAndView<User> velocity() {
+        return new ModelAndView<>("velocity/home", new User("Iván", true));
+    }
+
+    @View("/velocity/home")
+    @Get("/velocity-view")
+    public HttpResponse<User> velocityPojoView() {
+        return HttpResponse.ok(new User("Iván", true));
     }
 }
