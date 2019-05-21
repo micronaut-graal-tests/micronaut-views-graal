d056891ed6a1d95f3b5a06f12d293811f6821f81
Iván López <lopez.ivan@gmail.com>
2019-05-21 14:51:39 +0200

    Use only thymeleaf

--- src/main/java/example/micronaut/ViewsController.java
+++ src/main/java/example/micronaut/ViewsController.java
@@ -1,14 +1,23 @@
 package example.micronaut;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.ModelAndView;
+import io.micronaut.views.View;
 
 @Controller("/views")
 public class ViewsController {
 
-    @Get("/")
-    public ModelAndView<User> modelAndView() {
-        return new ModelAndView<>("dir/home", new User("Iván", true));
+    @Get("/thymeleaf")
+    public ModelAndView<User> thymeleaf() {
+        return new ModelAndView<>("thymeleaf/dir/home", new User("Iván", true));
     }
+
+    @Get("/thymeleaf-view")
+    @View("/thymeleaf/dir/home")
+    public HttpResponse<User> thymeleafPojoView() {
+        return HttpResponse.ok(new User("Iván", true));
+    }
+
 }
