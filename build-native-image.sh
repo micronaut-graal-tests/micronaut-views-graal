e3bd9445b38a16553130e8dd3c15340eb9d463f7
Iván López <lopez.ivan@gmail.com>
2019-06-05 17:38:21 +0200

    Use graal configuration options defined in Micronaut

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,3 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/views-graal-*.jar -H:+ReportExceptionStackTraces \
-    --initialize-at-build-time=sun.security.provider.NativePRNG
+native-image --no-server --no-fallback --class-path build/libs/views-graal-*.jar
