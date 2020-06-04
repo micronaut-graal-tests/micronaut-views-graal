811462664a7a5167787a530b7f12c251f535d2f4
Iván López <lopezi@objectcomputing.com>
2020-06-04 16:01:21 +0200

    Remove unnecessary GraalVM configuration

--- src/main/java/example/micronaut/Application.java
+++ src/main/java/example/micronaut/Application.java
@@ -1,24 +1,7 @@
 package example.micronaut;
 
-import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
 
-@TypeHint(value = {
-        org.apache.velocity.runtime.resource.ResourceManagerImpl.class,
-        org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader.class,
-        org.apache.velocity.runtime.resource.ResourceCacheImpl.class,
-        org.apache.velocity.runtime.ParserPoolImpl.class,
-        org.apache.velocity.runtime.directive.Break.class,
-        org.apache.velocity.runtime.directive.Define.class,
-        org.apache.velocity.runtime.directive.Evaluate.class,
-        org.apache.velocity.runtime.directive.Foreach.class,
-        org.apache.velocity.runtime.directive.Include.class,
-        org.apache.velocity.runtime.directive.Macro.class,
-        org.apache.velocity.runtime.directive.Parse.class,
-        org.apache.velocity.runtime.directive.Stop.class,
-        org.apache.velocity.util.introspection.ConversionHandlerImpl.class,
-        org.apache.velocity.util.introspection.UberspectImpl.class},
-        accessType = TypeHint.AccessType.ALL_PUBLIC)
 public class Application {
 
     public static void main(String[] args) {
